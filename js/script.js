0397c684f85e30c9847bfe459535dac801e33839
brodyaha <43912437+brodyaha@users.noreply.github.com>
2022-10-26 16:12:04 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -235,6 +235,19 @@ $(document).ready(function() {
     });
 });
 
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for(let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
+
 "use strict"
 
 let popupToggle = document.getElementById ('myBtn'),
